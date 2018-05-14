8ad31f9bbbc760743664d89f07437653eb82e5cd
veronicab1 <bartolucci.veronica@yahoo.it>
2018-05-14 16:32:38 +0200

    - Layout per la selezione del Piano - scorrimento orizzontale

--- app/src/main/java/ids/univpm/breakout/view/SelPiano.java
+++ app/src/main/java/ids/univpm/breakout/view/SelPiano.java
@@ -19,6 +19,12 @@ class SelPiano extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.navigation2);
+
+        non_connesso_txt=(TextView) findViewById(R.id.non_connesso_txt);
+        non_connesso= findViewById(R.id.non_connesso);
+        connesso_txt=(TextView) findViewById(R.id.connesso_txt);
+        connesso= findViewById(R.id.connesso);
+
         //Check connessione
         //se non connesso: Server connection: disconnected, simbolo X
         non_connesso.setVisibility(View.VISIBLE);
@@ -27,6 +33,7 @@ class SelPiano extends AppCompatActivity {
         // se connesso: connected: simbolo verde
         //connesso.setVisibility(View.VISIBLE);
         //connesso_txt.setVisibility(View.VISIBLE);
+
 
 
     }
